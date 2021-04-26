d2f6900ab30019451b5d3834e06526b0edd50490
Ilayda <ilayda.bakare@gmail.com>
2021-04-26 13:19:46 +0100

    First commit

---
+++ android/app/src/main/kotlin/com/ilaydaob/im_a_cat/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.ilaydaob.im_a_cat
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+Color darkOrange = const Color(0xFFc66900);
+Color midOrange = const Color(0xFFff9800);
+Color lightOrange = const Color(0xFFffc947);
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: darkOrange,
+          title: Center(child: Text("I'm a cat and that's that")),
+        ),
+        bottomNavigationBar: BottomAppBar(
+          color: Colors.blue,
+        ),
+        backgroundColor: midOrange,
+        body: Center(
+            child: Column(
+          children: <Widget>[
+            Expanded(
+                child: Image.asset(
+              'images/Cat-Instagram.jpg',
+            )),
+            Text("Gosh dang it, they're so cute"),
+            Expanded(
+                child: Image.asset(
+              'images/kitty_teeth.jpg',
+            )),
+          ],
+        )),
+      ),
+    ),
+  );
+}
